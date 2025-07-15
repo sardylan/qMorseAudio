60dcebfceb15e86272791ea7751603208bbe50b9
Luca Cireddu <sardylan@gmail.com>
2025-07-15 20:07:06 +0200

    fix(morseaudio): implement CMake variable for all OSes

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -4,9 +4,19 @@ set(APPLICATION_VERSION "${PROJECT_VERSION}")
 set(ORGANIZATION_NAME "The HellNet.org")
 set(ORGANIZATION_DOMAIN "thehellnet.org")
 
-execute_process(COMMAND date -u -Iseconds
-        OUTPUT_VARIABLE APPLICATION_BUILD
-        ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+if (UNIX)
+    execute_process(COMMAND date -u +%Y-%m-%dT%H:%M:%SZ
+            OUTPUT_VARIABLE APPLICATION_BUILD
+            ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+elseif (WIN32)
+    execute_process(COMMAND powershell -Command "Get-Date -Format 'yyyy-MM-ddTHH:mm:ssZ'"
+            OUTPUT_VARIABLE APPLICATION_BUILD
+            ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+else ()
+    message(ERROR "Unsupported platform for build date generation")
+    set(APPLICATION_BUILD "Unknown")
+endif ()
+
 message(STATUS "APPLICATION_BUILD: ${APPLICATION_BUILD}")
 
 execute_process(COMMAND git describe --match=NeVeRmAtCh --always --abbrev=7 --dirty
